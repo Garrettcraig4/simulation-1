4290295e2f515749c5c303696918ba9de7199290
Garrett <Garrettcrg@gmail.com>
2018-02-09 13:49:44 -0600

    server working

--- simulation-1/src/App.js
+++ simulation-1/src/App.js
@@ -4,11 +4,14 @@ import "./App.css";
 import axios from "axios";
 import { Switch, Route } from "react-router-dom";
 import routes from "./routes";
+import Header from "./Components/Header";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header" />
+
+        <Header />
       </div>
     );
   }
--- simulation-1/src/Components/Header.js
+++ simulation-1/src/Components/Header.js
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-test(){
-    axios.get(`/api/test`).then(response => {
-        console.log(response.data);
-    });
+class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //things to change
+    };
+  }
+
+  // test(){
+  //     axios.get(`/api/test`).then(response => {
+  //         console.log(response.data);
+  //     });
+  // }
+
+  render() {
+    return (
+      <div className="Header">
+        <h1> Heading</h1>
+      </div>
+    );
+  }
 }
+export default Header;
